e00b129393b2dd6dac4247d6e681c5d5d2f1e04c
skye <skyeladham@gmail.com>
2025-08-18 18:34:10 -0300

    visualization of what i need to do (not correct)

--- src/main.c
+++ src/main.c
@@ -14,8 +14,40 @@ int main() {
         0,0,0,0,0,0,0,0,0,0
     };
 
+    noiseMap[3][5] = 3;
 
-    noiseMap[3][5] = 1;
+    for(int i = 0; i<10; i++) {
+        for(int j = 0; j<10; j++) {
+            if(noiseMap[i][j] == 3) {
+                //if() {
+                    noiseMap[i-1][j-1] = 2;
+                    noiseMap[i+1][j+1] = 2;
+                    noiseMap[i-1][j+1] = 2;
+                    noiseMap[i+1][j-1] = 2;
+                    noiseMap[i+1][j] = 2;
+                    noiseMap[i-1][j] = 2;
+                    noiseMap[i][j+1] = 2;
+                    noiseMap[i][j-1] = 2;
+                //
+            }
+        }
+    }
+    for(int i = 0; i<10; i++) {
+        for(int j = 0; j<10; j++) {
+            if(noiseMap[i][j] == 2) {
+                //if() {
+                    noiseMap[i-1][j-1] = 1;
+                    noiseMap[i+1][j+1] = 1;
+                    noiseMap[i-1][j+1] = 1;
+                    noiseMap[i+1][j-1] = 1;
+                    noiseMap[i+1][j] = 1;
+                    noiseMap[i-1][j] = 1;
+                    noiseMap[i][j+1] = 1;
+                    noiseMap[i][j-1] = 1;
+                //}
+            }
+        }
+    }
 
     for(int i = 0; i<10; i++) {
         for(int j = 0; j<10; j++){
