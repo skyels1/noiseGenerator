66650f15858d15d16d3aa10d4e1bebea8c318cea
skye <skyeladham@gmail.com>
2025-08-19 00:36:18 -0300

    made it random

--- src/main.c
+++ src/main.c
@@ -1,35 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 int main() {
-    int noiseMap[10][10] = {
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0
-    };
+    int noiseMap[100][100];
+    srand(time(NULL));
 
-    noiseMap[3][5] = 3;
+    for(int i = 0; i<100; i++) {
+        for(int j = 0; j<100; j++) {
 
+            if(rand() % 100 < 5) {
+                noiseMap[i][j] = 3;
+            }
+            else if(rand() % 100 < 10) {
+                noiseMap[i][j] = 2;
+            }
+            else {
+                noiseMap[i][j] = 0;
+            }
+        }
+    }
+
+
+
+
+    //noiseMap[3][5] = 3;
+/*
     int directions[8][2] = {
         {-1,-1},{-1,0},{-1,1},
         {0,-1},{0,1},
         {1,-1},{1,0},{1,1}
     };
+    */
+    int directions[4][2] = {
+        {-1,0},
+        {0,-1},{0,1},
+        {1,0}
+    };
 
 
-    for(int i = 0; i<10; i++) {
-        for(int j = 0; j<10; j++) {
+    for(int i = 0; i<100; i++) {
+        for(int j = 0; j<100; j++) {
             if(noiseMap[i][j] == 3) {
-                for(int d = 0; d<8; d++) {
+                for(int d = 0; d<4; d++) {
                     int Oi = i + directions[d][0];
                     int Oj = j + directions[d][1];
-                    if(Oi >= 0 && Oi <= 10 && Oj >= 0 && Oj <=10) {
+                    if(Oi >= 0 && Oi < 100 && Oj >= 0 && Oj <100) {
                         if(noiseMap[Oi][Oj] < 2) {
                             noiseMap[Oi][Oj] = 2;
                         }
@@ -38,13 +55,13 @@ int main() {
             }
         }
     }
-    for(int i = 0; i<10; i++) {
-        for(int j = 0; j<10; j++) {
+    for(int i = 0; i<100; i++) {
+        for(int j = 0; j<100; j++) {
             if(noiseMap[i][j] == 2) {
-                for(int d = 0; d<8; d++) {
+                for(int d = 0; d<4; d++) {
                     int Oi = i + directions[d][0];
                     int Oj = j + directions[d][1];
-                    if(Oi >= 0 && Oi <= 10 && Oj >= 0 && Oj <=10) {
+                    if(Oi >= 0 && Oi < 100 && Oj >= 0 && Oj <100) {
                         if(noiseMap[Oi][Oj] < 1) {
                             noiseMap[Oi][Oj] = 1;
                         }
@@ -55,8 +72,8 @@ int main() {
     }
     
 
-    for(int i = 0; i<10; i++) {
-        for(int j = 0; j<10; j++){
+    for(int i = 0; i<100; i++) {
+        for(int j = 0; j<100; j++){
             printf("%i", noiseMap[i][j]);
         }
         printf("\n");
