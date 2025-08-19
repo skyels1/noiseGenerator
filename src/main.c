80556fa0fa7b0582660c7e167a0e4df0096f2c1a
skye <skyeladham@gmail.com>
2025-08-18 22:11:25 -0300

    corrected overwriting

--- src/main.c
+++ src/main.c
@@ -16,38 +16,44 @@ int main() {
 
     noiseMap[3][5] = 3;
 
+    int directions[8][2] = {
+        {-1,-1},{-1,0},{-1,1},
+        {0,-1},{0,1},
+        {1,-1},{1,0},{1,1}
+    };
+
+
     for(int i = 0; i<10; i++) {
         for(int j = 0; j<10; j++) {
             if(noiseMap[i][j] == 3) {
-                //if() {
-                    noiseMap[i-1][j-1] = 2;
-                    noiseMap[i+1][j+1] = 2;
-                    noiseMap[i-1][j+1] = 2;
-                    noiseMap[i+1][j-1] = 2;
-                    noiseMap[i+1][j] = 2;
-                    noiseMap[i-1][j] = 2;
-                    noiseMap[i][j+1] = 2;
-                    noiseMap[i][j-1] = 2;
-                //
+                for(int d = 0; d<8; d++) {
+                    int Oi = i + directions[d][0];
+                    int Oj = j + directions[d][1];
+                    if(Oi >= 0 && Oi <= 10 && Oj >= 0 && Oj <=10) {
+                        if(noiseMap[Oi][Oj] < 2) {
+                            noiseMap[Oi][Oj] = 2;
+                        }
+                    }
+        }
             }
         }
     }
     for(int i = 0; i<10; i++) {
         for(int j = 0; j<10; j++) {
             if(noiseMap[i][j] == 2) {
-                //if() {
-                    noiseMap[i-1][j-1] = 1;
-                    noiseMap[i+1][j+1] = 1;
-                    noiseMap[i-1][j+1] = 1;
-                    noiseMap[i+1][j-1] = 1;
-                    noiseMap[i+1][j] = 1;
-                    noiseMap[i-1][j] = 1;
-                    noiseMap[i][j+1] = 1;
-                    noiseMap[i][j-1] = 1;
-                //}
+                for(int d = 0; d<8; d++) {
+                    int Oi = i + directions[d][0];
+                    int Oj = j + directions[d][1];
+                    if(Oi >= 0 && Oi <= 10 && Oj >= 0 && Oj <=10) {
+                        if(noiseMap[Oi][Oj] < 1) {
+                            noiseMap[Oi][Oj] = 1;
+                        }
+                    }
+        }
             }
         }
     }
+    
 
     for(int i = 0; i<10; i++) {
         for(int j = 0; j<10; j++){
